7a7239173b27ef897bc77723b0255e5354256d11
poojapatil0195 <101088536+poojapatil0195@users.noreply.github.com>
2022-03-25 16:35:24 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,37 @@
+/*
+This is to create security group (port 80 inbound and all trafic outbound)
+*/
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http_module"
+  description = "Allow http inbound traffic"
+  vpc_id      = "vpc-18824b73"
+
+  ingress {
+    description = "http from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "ssh from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
